6ac66c5fa4821e72e721a2c864d5f7b9f65f0065
ang <angiezafra07@gmail.com>
2021-02-18 19:39:01 -0500

    Warm ups

--- JS-algorithms.js
+++ JS-algorithms.js
@@ -56,3 +56,13 @@ function sumDouble(a, b){
  return sum
 
 }
+
+
+// Warmup-1 -- diff21
+// Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+// Examples
+
+// diff21(19) → 2
+// diff21(10) → 11
+// diff21(21) → 0
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # javascript-algos
+### [CodingJS](https://the-winter.github.io/codingjs/)
 ## practicing  Javascript Algorithms
