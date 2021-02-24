211ec90a96cda9378f1a71db1470f120dad55038
ang <angiezafra07@gmail.com>
2021-02-23 20:02:17 -0500

    still working on algorithms

--- JS-algorithms.js
+++ JS-algorithms.js
@@ -92,5 +92,24 @@ function diff21(n){
     return true
   else if (talking <= 20)
     return false
+  //  why am i having  a hard time with logic?
+ }
+
+
+//  Warmup-1 -- makes10
+// Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+// Examples
+
+// makes10(9, 10) → true
+// makes10(9, 9) → false
+// makes10(1, 9) → true
+
+
+function makes10(a, b){
   
+  if ( a <= 10 || b <= 10 || a+b <= 10 ){
+    return true                    
+ }
+  // this answer only passes like 7 our of the 9 tests
 }
